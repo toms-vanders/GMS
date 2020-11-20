ac90478d00975053eed3658246e4e383f721315b
dymus <damian.hrabaszcz@op.pl>
2020-11-20 13:42:55 +0100

    Modified UI and algorithm for searching events in the web client, added buttons for every events.

--- GMS/GMS - Web Client/JavaScript/search-table-control.js
+++ GMS/GMS - Web Client/JavaScript/search-table-control.js
@@ -16,9 +16,10 @@
                 if ((eventTypes.includes(obj.eventType)) || (eventTypes.length === 0)) {
                     if (keywords.some(v => obj.name.toLowerCase().includes(v)) || keywords.some(v => obj.description.toLowerCase().includes(v)) || (keywords.length === 0)) {
                         trHTML += "<tr>";
+                        const keys = Object.keys(obj);
+                        console.log(keys);
                         Object.entries(obj).forEach(([key, value]) => {
                             if (key === "participants" || key === "waitingList" || key === "guildID" || key === "rowId") {
-
                             } else if (key === "eventID") {
                                 trHTML += "<th scope=\"row\">" + value + "</th>"
                             } else if (key === "date") {
@@ -26,6 +27,13 @@
                                 trHTML += "<td>" + date.toUTCString() + "</td>";
                             } else {
                                 trHTML += "<td>" + value + "</td>";
+                            }
+                            if (Object.is(keys.length - 1, keys.indexOf(key))) {
+                                trHTML += "<td>";
+                                trHTML += "<button type=\"button\" class=\"btn btn-success btn-sm\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i></button> ";
+                                trHTML += "<button type=\"button\" class=\"btn btn-warning btn-sm\"><i class=\"fa fa-pencil\" aria-hidden=\"true\"></i></button> ";
+                                trHTML += "<button type=\"button\" class=\"btn btn-danger btn-sm\"><i class=\"fa fa-trash-o\" aria-hidden=\"true\"></i></button>";
+                                trHTML += "</td>";
                             }
                         });
                     }
@@ -42,5 +50,5 @@
 
 function clearTable() {
     var events = document.getElementById("events-table");
-    events.innerHTML = "<tr><th scope=\"col\">ID</th><th scope=\"col\">Name</th><th scope=\"col\">Event type</th><th scope=\"col\">Location</th><th scope=\"col\">Date</th><th scope=\"col\">Description</th><th scope=\"col\">Max. num. of character</th></tr>"
+    events.innerHTML = "<tr><th scope=\"col\">ID</th><th scope=\"col\">Name</th><th scope=\"col\">Event type</th><th scope=\"col\">Location</th><th scope=\"col\">Date</th><th scope=\"col\">Description</th><th scope=\"col\">Max. num. of character</th><th scope=\"col\" style=\"width: 12%;\">Actions</th></tr>"
 }
