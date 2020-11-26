27a0e428e40591b10409fda54c017495c636254d
Adrian <d.michael.adrian@gmail.com>
2020-11-26 11:07:26 +0100

    HomeController refactored and split

---
+++ GMS/GMS - Web Client/Controllers/EventController.cs
@@ -0,0 +1,99 @@
+﻿using GMS___Model;
+using GMS___Web_Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GMS___Web_Client.Controllers
+{
+    public class EventController : AuthController
+    {
+        // GET: Event
+
+        public ActionResult CreateEventForm()
+        {
+            if (InSession())
+            {
+                // Getting all event types needed for DropDownList
+                var eventTypes = GetAllEventTypes();
+                var model = new EventModel();
+                // Get list of SelectListItem(s)
+                model.EventTypes = GetOptionEventTypesList(eventTypes);
+                ViewBag.Message = "Creating event.";
+                return View(model);
+            }
+            ViewBag.Error = "You aren't authorized to access this page.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult SearchEvents(string name)
+        {
+            if (this.Session["Username"] != null)
+            {
+                string urlSuffix = "gw2api/characters/" + name + "/core";
+                ViewBag.Character = GetJson<Character>(urlSuffix, new Character());
+                // Getting all event types needed for DropDownList
+                var eventTypes = GetAllEventTypes();
+                var model = new EventModel();
+                // Get list of SelectListItem(s)
+                model.EventTypes = GetOptionEventTypesList(eventTypes);
+                ViewBag.Message = "Find new events";
+                return View(model);
+            }
+            ViewBag.Error = "You aren't authorized to access this page.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEventForm(EventModel model)
+        {
+            if (InSession())
+            {
+                // Get all eventTypes
+                var eventTypes = GetAllEventTypes();
+                // Set these eventTypes on the model. Needs to be done because
+                // only the selected value from DropDownList is posed back, not the whole
+                // list of EventType(s)
+                model.EventTypes = GetOptionEventTypesList(eventTypes);
+
+                if (ModelState.IsValid)
+                {
+                    Event tempEvent = PostJson("api/guild/events/insert", new Event(model.EventName, model.EventType,
+                        model.EventLocation, model.EventDateTime, model.EventDescription,
+                        model.EventMaxNumberOfCharacters, "116E0C0E-0035-44A9-BB22-4AE3E23127E5"));
+                    if (tempEvent != null)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ViewBag.Error = "Invalid information was given.";
+                return View(model);
+            } else
+            {
+                ViewBag.Error = "You aren't authorized to access this page.";
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        private IEnumerable<string> GetAllEventTypes()
+        {
+            return Enum.GetNames(typeof(EventType.EventTypes)).ToList();
+        }
+
+        private IEnumerable<SelectListItem> GetOptionEventTypesList(IEnumerable<string> elements)
+        {
+            var eventTypeList = new List<SelectListItem>();
+            foreach (var element in elements)
+            {
+                eventTypeList.Add(new SelectListItem
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+            return eventTypeList;
+        }
+    }
+}
--- GMS/GMS - Web Client/Controllers/UserController.cs
+++ GMS/GMS - Web Client/Controllers/UserController.cs
@@ -1,17 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using GMS___Model;
+using GMS___Web_Client.Models;
+using System;
+using System.Collections;
 using System.Web.Mvc;
 
 namespace GMS___Web_Client.Controllers
 {
-    public class UserController : Controller
+    public class UserController : AuthController
     {
         // GET: User
-        public ActionResult Index()
+
+        public ActionResult UserPage()
         {
+            if (InSession())
+            {
+                this.Session["Guild"] = "";
+                ArrayList characterList = new ArrayList();
+                ArrayList characterNameList = GetJson<ArrayList>("gw2api/characters", new ArrayList());
+                foreach (string name in characterNameList)
+                {
+                    string urlSuffix = "gw2api/characters/" + name + "/core";
+                    characterList.Add(GetJson<Character>(urlSuffix, new Character()));
+                }
+                ViewBag.Characters = characterList;
+                ViewBag.Message = "Your user page.";
+                return View();
+            }
+            ViewBag.Error = "You aren't authorized to access this page.";
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult CharacterPage(string name)
+        {
+            if (InSession())
+            {
+                string urlSuffix = "gw2api/characters/" + name;
+                ViewBag.Character = GetJson<Character>(urlSuffix + "/core", new Character());
+                ViewBag.Message = "Your character page.";
+                ViewBag.Equipment = InitializeEquipment(GetJson<Equipments>(urlSuffix + "/equipment", new Equipments()));
+                this.Session["Guild"] = ViewBag.Character.Guild;
+                return View();
+            }
+            ViewBag.Error = "You aren't authorized to access this page.";
+            return RedirectToAction("Index","Home");
+        }
+        public ActionResult ApiForm()
+        {
+            if (InSession())
+            {
+                ViewBag.Message = "Your API form.";
+                return View();
+            }
+            ViewBag.Error = "You aren't authorized to access this page.";
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ApiForm(ApiModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User();
+                user.EmailAddress = this.Session["EmailAddress"].ToString();
+                user.ApiKey = model.ApiKey;
+                if (PostJson("api/user/insertapi", user) != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ViewBag.Error = "Invalid information was given.";
             return View();
+        }
+
+        public ArrayList InitializeEquipment(Equipments jsonList)
+        {
+            ArrayList equipment = new ArrayList();
+            foreach (EquipmentSlot item in jsonList.Equipment)
+            {
+                try
+                {
+                    equipment.Add(GetJson<Item>("gw2api/items/" + item.Id, new Item()));
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return equipment;
         }
     }
 }
