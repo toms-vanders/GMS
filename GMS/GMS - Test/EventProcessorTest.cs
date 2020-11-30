6004c0a95a8d304260e658fe4cd37fbb15c5d199
Toms Vanders <toms.vanders@gmail.com>
2020-11-30 14:02:37 +0100

    Sprint merge

--- GMS/GMS - Test/EventProcessorTest.cs
+++ GMS/GMS - Test/EventProcessorTest.cs
@@ -10,22 +10,21 @@ using System.Linq;
 
 namespace GMS___Test
 {
-    /// <summary>
-    /// Summary description for EventProcessorTest
-    /// </summary>
     [TestClass]
     public class EventProcessorTest
     {
+        EventAccess ea;
         EventProcessor ep;
         Event testEvent;
 
         [TestInitialize]
         public void TestInitialize()
         {
+            ea = new EventAccess();
             ep = new EventProcessor();
             
             string name = "Test Raid";
-            string eventType = "Test Event";
+            string eventType = "Raid";
             string location = "266,070";
             DateTime date = new DateTime(2020, 12, 10, 12, 0, 0);
             string description = "Doing a full raid. Be available for at least 2 hours";
@@ -37,77 +36,112 @@ namespace GMS___Test
         }
 
         [TestMethod]
-        public void EventProcessorIntegrationTest()
+        public void TestEventCreation()
         {
-            CreateValidEvent();
-            GetAllGuildEventListFromDatabase();
-            GetAllGuildEventsByEventTypeFromDatabase();
-            GetGuildEventByEventIDFromDatabase();
-            DeleteExistingEvent();
+            Boolean InsertCompleted = false;
+            Boolean TestThrewException = false;
+            try
+            {
+                InsertCompleted = ep.InsertEvent(testEvent.Name, testEvent.EventType, testEvent.Location,
+                testEvent.Date, testEvent.Description, testEvent.MaxNumberOfCharacters, testEvent.GuildID);
+            }
+            catch(Exception ex)
+            {
+                TestThrewException = true;
+            }
+            finally
+            {
+                //Cleanup
+                int id = ea.getIdOfEvent(testEvent.Name);
+                ea.DeleteEventByID(id);
+            }
+
+            Assert.IsTrue(InsertCompleted);
+            Assert.IsFalse(TestThrewException);
         }
 
-        public void CreateValidEvent()
+        [TestMethod]
+        public void TestGettingEvents()
         {
+            int firstResult = -1;
+            int secondResult = -1;
+            int thirdResult = -1;
+            string name1 = "";
+            string name2 = "";
+            Boolean TestThrewException = false;
+            try
+            {
+                ep.InsertEvent(testEvent.Name, testEvent.EventType, testEvent.Location,
+                testEvent.Date, testEvent.Description, testEvent.MaxNumberOfCharacters, testEvent.GuildID);
 
-            // Act and Assert
-            Assert.IsTrue(ep.InsertEvent(testEvent.Name, testEvent.EventType, testEvent.Location, 
-                testEvent.Date, testEvent.Description, testEvent.MaxNumberOfCharacters, testEvent.GuildID));
+                List<Event> firstSearch = (List<Event>)ep.GetAllGuildEvents(testEvent.GuildID);
+                List<Event> secondSearch = (List<Event>)ep.GetAllGuildEventsByEventType(testEvent.GuildID, testEvent.EventType);
+                List<Event> thirdSearch = (List<Event>)ep.GetAllGuildEventsByEventType(testEvent.GuildID, "Super Raid");
+
+                firstResult = firstSearch.Count();
+                secondResult = secondSearch.Count();
+                thirdResult = thirdSearch.Count();
+                name1 = firstSearch[0].Name;
+                name2 = secondSearch[0].Name;
+            }
+            catch (Exception ex)
+            {
+                TestThrewException = true;
+            }
+            finally
+            {
+                //Cleanup
+                int id = ea.getIdOfEvent(testEvent.Name);
+                ea.DeleteEventByID(id);
+            }
+
+            Assert.AreEqual(1, firstResult);
+            Assert.AreEqual(1, secondResult);
+            Assert.AreEqual(0, thirdResult);
+            Assert.AreEqual(testEvent.Name, name1);
+            Assert.AreEqual(testEvent.Name, name2);
+            Assert.IsFalse(TestThrewException);
         }
 
-        public void GetAllGuildEventListFromDatabase()
+        [TestMethod]
+        public void TestUpdate()
         {
+            Event event1 = new Event();
+            bool test1 = false;
+            bool test2 = true;
+            Boolean TestThrewException = false;
+            try
+            {
+                ep.InsertEvent(testEvent.Name, testEvent.EventType, testEvent.Location,
+                testEvent.Date, testEvent.Description, testEvent.MaxNumberOfCharacters, testEvent.GuildID);
+                int id = ea.getIdOfEvent(testEvent.Name);
+                Event eventToBeUpdated = ((List<Event>)ep.GetEventByID(id))[0];
+                eventToBeUpdated.Location = "Update";
+                test1 = ep.UpdateEvent(eventToBeUpdated.EventID, eventToBeUpdated.Name, eventToBeUpdated.EventType, 
+                    eventToBeUpdated.Location,eventToBeUpdated.Date, eventToBeUpdated.Description, 
+                    eventToBeUpdated.MaxNumberOfCharacters, eventToBeUpdated.GuildID, eventToBeUpdated.RowId);
+                //Since the rowID wasnt updated this update should fail
+                eventToBeUpdated.Location = "Another update";
+                test2 = ep.UpdateEvent(eventToBeUpdated.EventID, eventToBeUpdated.Name, eventToBeUpdated.EventType,
+                    eventToBeUpdated.Location, eventToBeUpdated.Date, eventToBeUpdated.Description,
+                    eventToBeUpdated.MaxNumberOfCharacters, eventToBeUpdated.GuildID, eventToBeUpdated.RowId);
+                event1 = ((List<Event>)ep.GetEventByID(id))[0];
+            }
+            catch (Exception ex)
+            {
+                TestThrewException = true;
+            }
+            finally
+            {
+                //Cleanup
+                int id = ea.getIdOfEvent(testEvent.Name);
+                ea.DeleteEventByID(id);
+            }
 
-            // Arrange
-
-            // Act
-            List<Event> returnList = (List<Event>)ep.GetAllGuildEvents(testEvent.GuildID);
-            Event returnedEvent = returnList.FirstOrDefault();
-            // Assert
-            Assert.AreEqual(testEvent.GuildID, returnedEvent.GuildID);
-        }
-
-        public void GetAllGuildEventsByEventTypeFromDatabase()
-        {
-
-            // Arrange
-
-            // Act
-            List<Event> returnList = (List<Event>)ep.GetAllGuildEventsByEventType(testEvent.GuildID, testEvent.EventType);
-            Event returnedEvent = returnList.FirstOrDefault();
-            // Assert
-            Assert.AreEqual(testEvent.EventType, returnedEvent.EventType);
-        }
-
-        public void GetGuildEventByEventIDFromDatabase()
-        {
-            // Assert
-            List<Event> returnListByGuild = (List<Event>)ep.GetAllGuildEvents(testEvent.GuildID);
-            Event testEventWithID = returnListByGuild.FirstOrDefault();
-
-            // Act
-            List<Event> returnListByID = (List<Event>)ep.GetEventByID(testEventWithID.EventID);
-            Event returnedEvent = returnListByID.FirstOrDefault();
-            // Assert
-            Assert.AreEqual(returnedEvent.EventID, testEventWithID.EventID);
-        }
-
-
-        public void DeleteExistingEvent()
-        {
-
-            // Arrange
-            List<Event> returnListByGuild = (List<Event>)ep.GetAllGuildEvents(testEvent.GuildID);
-            Event testEventWithID = returnListByGuild.FirstOrDefault();
-
-            // Act and Assert
-
-            Assert.IsTrue(ep.DeleteEventByID(testEventWithID.EventID));
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-            //Should be implemented more specifically to delete test data from db
+            Assert.AreEqual("Update",event1.Location);
+            Assert.IsTrue(test1);
+            Assert.IsFalse(test2);
+            Assert.IsFalse(TestThrewException);
         }
     }
 }
